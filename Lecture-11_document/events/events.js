6569208d0157e1c206a02aaa5035d3e65335f6e5
Jasbir96 <mr.jasbirsingh96@gmail.com>
2024-06-24 23:12:44 +0530

    Lecture 11 : completed todo app

---
+++ Lecture-11_document/events/events.js
@@ -0,0 +1,16 @@
+// const button = document.querySelector("button");
+// const box = document.querySelector(".box");
+
+
+// // button.onclick=function(){
+// //     alert("Button was clicked");
+// // }
+
+// const colors = ["lightblue", "lightgreen", "cyan", "gray", "red"]
+// let i = 0;
+// button.addEventListener("click", function () {
+//     i = i % colors.length;
+//     box.style.backgroundColor = colors[i]
+//     i++;
+// }
+// )
