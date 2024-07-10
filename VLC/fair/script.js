16f7da23f63bfe542bd4de7dbb831062fbf6bead
Jasbir96 <mr.jasbirsingh96@gmail.com>
2024-07-10 22:21:27 +0530

    Lecture-17 : added  controls and poc for toast

--- VLC/fair/script.js
+++ VLC/fair/script.js
@@ -1,49 +1,111 @@
-const speedUp = document.querySelector("#speedUp");
-const speedDown = document.querySelector("#speedDown");
-const volumeUp = document.querySelector("#volumeUp");
-const volumeDown = document.querySelector("#volumeDown");
+
 
 const videoBtn = document.querySelector("#videoBtn");
 const videoInput = document.querySelector("#videoInput");
-
 const videoPlayer = document.querySelector("#main");
 
-const speedUpHandler = () => {
-    // alert("Speed up was clicked");
-    const videoElement = document.querySelector(".main .video");
-    if (isVideoPresent !== null) {
-        // video is present
-        videoElement.defaultPlaybackRate = videoElement.defaultPlaybackRate + 0.1;
-    }
-}
 
 const handleInput = () => {
-    console.log("Input is clicked");
+    // console.log("Input is clicked");
     // you have make it click
     videoInput.click();
-
 }
 const acceptInputHandler = (obj) => {
+    // to get file selected
     const selectedVideo = obj.target.files[0];
     //  src -> base64 
     const link = URL.createObjectURL(selectedVideo);
+    // console.log(link);
 
     const videoElement = document.createElement("video");
     videoElement.src = link;
     // now it is done
     videoElement.setAttribute("class", "video");
     videoPlayer.appendChild(videoElement);
+    // videoElement.controls = true;
     videoElement.play();
-    videoElement.volume = 70;
-
+    videoElement.volume = 0.3;
     // how to inc/dec the volume of a video in js 
     // how to inc/dec speed of a video in js 
 }
 
-
-
-speedUp.addEventListener("click", speedUpHandler);
 videoBtn.addEventListener("click", handleInput);
-
 // when file is selected
-videoInput.addEventListener("change", acceptInputHandler)
+videoInput.addEventListener("change", acceptInputHandler);
+
+/*******************volume and speed*******************/
+// select the element
+const speedUp = document.querySelector("#speedUp");
+const speedDown = document.querySelector("#speedDown");
+const volumeUp = document.querySelector("#volumeUp");
+const volumeDown = document.querySelector("#volumeDown");
+
+const speedUpHandler = () => {
+    // * where is the video-> 
+    const videoElement = document.querySelector("video");
+    if (videoElement == null) {
+        return;
+    }
+    // i want that playback speed should be at max 300%
+    if (videoElement.playbackRate > 3) {
+        return;
+    }
+    // video -> speed increase
+    const increaseSpeed = videoElement.playbackRate + 0.5;
+    videoElement.playbackRate = increaseSpeed;
+    // which property you will use to increase it's speed 
+    // * how much you want to increase
+}
+const speedDownhandler = () => {
+    const videoElement = document.querySelector("video");
+    if (videoElement == null) {
+        return;
+    }
+    if (videoElement.playbackRate > 0) {
+        // video -> speed decrease
+        const decreasedSpeed = videoElement.playbackRate - 0.5;
+        videoElement.playbackRate = decreasedSpeed;
+        console.log("decreased speed", decreasedSpeed)
+    }
+
+}
+
+const volumeUpHandler = () => {
+    // select the video
+    const videoElement = document.querySelector("video");
+    if (videoElement == null) {
+        return;
+    }
+    // property to play with volume 
+    if (videoElement.volume >= 0.99) {
+        return;
+    }
+    videoElement.volume = videoElement.volume + 0.1;
+    console.log("increseas volume", videoElement.volume);
+
+}
+
+
+const volumeDownHandler = () => {
+    // select the video
+    const videoElement = document.querySelector("video");
+    if (videoElement == null) {
+        return;
+    }
+    // property to play with volume 
+    if (videoElement.volume<= 0.1) {
+        videoElement.volume=0;
+       return
+    }
+    videoElement.volume = videoElement.volume - 0.1;
+    console.log("decreased volume", videoElement.volume);
+}
+// identify on which event your logic should trigger
+speedUp.addEventListener("click", speedUpHandler);
+speedDown.addEventListener("click", speedDownhandler)
+volumeUp.addEventListener("click", volumeUpHandler);
+volumeDown.addEventListener("click", volumeDownHandler);
+
+
+
+
--- VLC/fair/todo.md
+++ VLC/fair/todo.md
@@ -7,3 +7,7 @@
 * Audio -> level increase and decrease 
 * full screen
   
+**you don't know a fact  -> search about the fact -> get some result -> do hit and trial**
+
+
+**CurrentTime**:  The currentTime property sets or returns the current position (in seconds) of the audio/video playback.
