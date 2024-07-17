cc0d45adbe41e6893c62e422df2beb1199525952
Jasbir96 <mr.jasbirsingh96@gmail.com>
2024-07-17 22:14:21 +0530

    VLC: added timer and seek behaviour in our input

--- VLC/fair/script.js
+++ VLC/fair/script.js
@@ -3,7 +3,14 @@
 const videoBtn = document.querySelector("#videoBtn");
 const videoInput = document.querySelector("#videoInput");
 const videoPlayer = document.querySelector("#main");
+const totalTimeElem = document.querySelector("#totalTime");
+const currentTimeElem = document.querySelector("#currentTime");
+const slider = document.querySelector("#slider");
 
+let video
+let duration;
+let timerObj;
+let currentPlayTime;
 
 const handleInput = () => {
     // console.log("Input is clicked");
@@ -28,16 +35,25 @@ const acceptInputHandler = (obj) => {
     }
     // now after the above check -> add the videoElement
     videoPlayer.appendChild(videoElement);
+    video = videoElement
     videoElement.play();
     videoElement.volume = 0.3;
     videoElement.addEventListener("loadedmetadata", function () {
-        // your time will there
+        // it gives in decimal value -> convert that into seconds
+        duration = Math.round(videoElement.duration);
+        // convert seconds into hrs:mins:secs
+        let time = timeFormat(duration);
+        totalTimeElem.innerText = time;
+        slider.setAttribute("max", duration);
+        startTimer();
+
     })
 }
 
 videoBtn.addEventListener("click", handleInput);
 // when file is selected
 videoInput.addEventListener("change", acceptInputHandler);
+
 
 /*******************volume and speed*******************/
 // select the element
@@ -139,3 +155,48 @@ const handleFullScreen = () => {
 
 const fullScreenElem = document.querySelector("#fullScreen");
 fullScreenElem.addEventListener("click", handleFullScreen)
+// adding seek behaviour in slider
+slider.addEventListener("change", function (e) {
+    let value = e.target.value;
+    video.currentTime = value;
+})
+
+
+
+/***************utility function to convert secs into hrs :mns : seconds*****************/
+function timeFormat(timeCount) {
+    let time = '';
+    const sec = parseInt(timeCount, 10);
+    let hours = Math.floor(sec / 3600);
+    let minutes = Math.floor((sec - (hours * 3600)) / 60);
+    let seconds = sec - (hours * 3600) - (minutes * 60);
+    if (hours < 10)
+        hours = "0" + hours;
+    if (minutes < 10)
+        minutes = "0" + minutes;
+    if (seconds < 10)
+        seconds = "0" + seconds
+    time = `${hours}:${minutes}:${seconds}`;
+    return time;
+}
+
+// function that runs the slider and timer  
+function startTimer() {
+    timerObj = setInterval(function () {
+        currentPlayTime = Math.round(video.currentTime);
+        slider.value = currentPlayTime;
+        const time = timeFormat(currentPlayTime);
+        currentTimeElem.innerText = time;
+        if (currentPlayTime == duration) {
+            state = "pause";
+            stopTimer();
+            video.remove();
+            slider.value = 0;
+            currentTimeElem.innerText = "00:00:00";
+            totalTimeElem.innerText = '--/--/--';
+        }
+    }, 1000);
+}
+function stopTimer() {
+    clearInterval(timerObj);
+}
